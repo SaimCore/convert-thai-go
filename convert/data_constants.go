e1651a1e2f69967c6d9be3a37d3fab74835d4bb2
PurinPintakhiew <dev.purin@gmail.com>
2024-08-28 00:51:02 +0700

    feat : data_constants

---
+++ convert/data_constants.go
@@ -0,0 +1,61 @@
+package convert
+
+var numberThConstants = map[string]string{
+	"0": "๐",
+	"1": "๑",
+	"2": "๒",
+	"3": "๓",
+	"4": "๔",
+	"5": "๕",
+	"6": "๖",
+	"7": "๗",
+	"8": "๘",
+	"9": "๙",
+	".": ".",
+	",": ",",
+	"-": "-",
+}
+
+var fullMonths = map[string]string{
+	"January":   "มกราคม",
+	"February":  "กุมภาพันธ์",
+	"March":     "มีนาคม",
+	"April":     "เมษายน",
+	"May":       "พฤษภาคม",
+	"June":      "มิถุนายน",
+	"July":      "กรกฎาคม",
+	"August":    "สิงหาคม",
+	"September": "กันยายน",
+	"October":   "ตุลาคม",
+	"November":  "พฤศจิกายน",
+	"December":  "ธันวาคม",
+}
+
+var shortMonths = map[string]string{
+	"January":   "ม.ค.",
+	"February":  "ก.พ.",
+	"March":     "มี.ค.",
+	"April":     "เม.ย.",
+	"May":       "พ.ค.",
+	"June":      "มิ.ย.",
+	"July":      "ก.ค.",
+	"August":    "ส.ค.",
+	"September": "ก.ย.",
+	"October":   "ต.ค.",
+	"November":  "พ.ย.",
+	"December":  "ธ.ค.",
+}
+
+var fullDays = map[string]string{
+	"Sunday":    "อาทิตย์",
+	"Monday":    "จันทร์",
+	"Tuesday":   "อังคาร",
+	"Wednesday": "พุธ",
+	"Thursday":  "พฤหัสบดี",
+	"Friday":    "ศุกร์",
+	"Saturday":  "เสาร์",
+}
+
+var shortDays = map[string]string{
+	"Sunday": "อา", "Monday": "จ", "Tuesday": "อ", "Wednesday": "พ", "Thursday": "พฤ", "Friday": "ศ", "Saturday": "ส",
+}
--- convert/number_format.go
+++ convert/number_format.go
@@ -5,22 +5,6 @@ import (
 	"strings"
 )
 
-var numberThConstants = map[string]string{
-	"0": "๐",
-	"1": "๑",
-	"2": "๒",
-	"3": "๓",
-	"4": "๔",
-	"5": "๕",
-	"6": "๖",
-	"7": "๗",
-	"8": "๘",
-	"9": "๙",
-	".": ".",
-	",": ",",
-	"-": "-",
-}
-
 func formatNumberWithComma(numberStr string) string {
 	var result []string
 
