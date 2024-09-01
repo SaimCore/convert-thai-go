f9b58b8c047395f6ae23abb9ce7c79ab58a1c627
PurinPintakhiew <dev.purin@gmail.com>
2024-09-01 22:52:25 +0700

    chore : number_format

--- number_format.go
+++ number_format.go
@@ -5,6 +5,22 @@ import (
 	"strings"
 )
 
+var numberThConstants = map[string]string{
+	"0": "๐",
+	"1": "๑",
+	"2": "๒",
+	"3": "๓",
+	"4": "๔",
+	"5": "๕",
+	"6": "๖",
+	"7": "๗",
+	"8": "๘",
+	"9": "๙",
+	".": ".",
+	",": ",",
+	"-": "-",
+}
+
 func formatNumberWithComma(numberStr string) string {
 	var result []string
 
