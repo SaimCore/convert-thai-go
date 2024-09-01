671f9ee777d1f21d84458b59161510f6b16a9412
PurinPintakhiew <dev.purin@gmail.com>
2024-09-01 22:52:04 +0700

    chore : date_format

--- date_format.go
+++ date_format.go
@@ -6,6 +6,50 @@ import (
 	"time"
 )
 
+var fullMonths = map[string]string{
+	"January":   "มกราคม",
+	"February":  "กุมภาพันธ์",
+	"March":     "มีนาคม",
+	"April":     "เมษายน",
+	"May":       "พฤษภาคม",
+	"June":      "มิถุนายน",
+	"July":      "กรกฎาคม",
+	"August":    "สิงหาคม",
+	"September": "กันยายน",
+	"October":   "ตุลาคม",
+	"November":  "พฤศจิกายน",
+	"December":  "ธันวาคม",
+}
+
+var shortMonths = map[string]string{
+	"January":   "ม.ค.",
+	"February":  "ก.พ.",
+	"March":     "มี.ค.",
+	"April":     "เม.ย.",
+	"May":       "พ.ค.",
+	"June":      "มิ.ย.",
+	"July":      "ก.ค.",
+	"August":    "ส.ค.",
+	"September": "ก.ย.",
+	"October":   "ต.ค.",
+	"November":  "พ.ย.",
+	"December":  "ธ.ค.",
+}
+
+var fullDays = map[string]string{
+	"Sunday":    "อาทิตย์",
+	"Monday":    "จันทร์",
+	"Tuesday":   "อังคาร",
+	"Wednesday": "พุธ",
+	"Thursday":  "พฤหัสบดี",
+	"Friday":    "ศุกร์",
+	"Saturday":  "เสาร์",
+}
+
+var shortDays = map[string]string{
+	"Sunday": "อา", "Monday": "จ", "Tuesday": "อ", "Wednesday": "พ", "Thursday": "พฤ", "Friday": "ศ", "Saturday": "ส",
+}
+
 func addZero(numberStr string) string {
 	if len(numberStr) == 3 {
 		return "๐" + numberStr
